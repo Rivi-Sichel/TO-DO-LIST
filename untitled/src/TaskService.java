70a58d76db7c510df613e4a741cfcc3483f679bb
Rivi-Sichel <rivizichel1@gmail.com>
2025-11-25 17:08:50 +0200

    add documentation to TaskService

--- untitled/src/TaskService.java
+++ untitled/src/TaskService.java
@@ -6,11 +6,13 @@ public class TaskService {
 
     private final TaskRepository repository;
 
+    //Creates a new instance of TaskService using the provided task repository.
     public TaskService(TaskRepository repository) {
         this.repository = repository;
     }
 
-    // סימון משימה כ-DONE
+    //change the status of the task to be marks
+    //get id's task and change the status
     public void markAsDone(int taskId) {
         Task task = repository.getById(taskId);
         if (task != null) {
@@ -19,7 +21,9 @@ public class TaskService {
         }
     }
 
-    // חיפוש משימות לפי טקסט בכותרת או בתיאור
+    //sort the tasks by text
+    //get text - the word to search
+    //return the result of the search
     public List<Task> search(String text) {
         List<Task> result = new ArrayList<>();
         for (Task t : repository.listAll()) {
@@ -31,7 +35,8 @@ public class TaskService {
         return result;
     }
 
-    // רשימת כל המשימות ממוינת לפי סטטוס: NEW → STARTED → DONE
+    //sort the tasks by status
+    //return all the tasks by status
     public List<Task> listSortedByStatus() {
         List<Task> tasks = new ArrayList<>(repository.listAll());
         tasks.sort(Comparator.comparing(Task::getStatus));
